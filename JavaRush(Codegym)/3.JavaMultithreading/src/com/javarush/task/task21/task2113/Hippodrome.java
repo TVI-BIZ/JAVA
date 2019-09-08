6e00d3809c48846fcd52a1cb97d1960743ddb9c2
TVI-BIZ <vladebay1@gmail.com>
2019-09-08 12:54:08 +0200

    07.09.2019 - 23 LVL JavaRush_CodeGym_14_Tasks_Solved

--- JavaRush(Codegym)/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ JavaRush(Codegym)/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -4,14 +4,69 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    private List<Horse> horses = new ArrayList<>();
+    private static List<Horse> horses = new ArrayList<>();
 
     public List<Horse> getHorses() {
         return horses;
     }
 
-    public static void main(String[] args){
+    public Hippodrome(List horses){
+        this.horses = horses;
+    }
 
+    public static Hippodrome game;
+    public static void main(String[] args) throws InterruptedException {
+
+        Horse horse1 = new Horse("Belka",3.0,0.0);
+        Horse horse2 = new Horse("Strelka",3.0,0.0);
+        Horse horse3 = new Horse("Reks",3.0,0.0);
+
+        horses.add(horse1);
+        horses.add(horse2);
+        horses.add(horse3);
+
+        game = new Hippodrome(horses);
+        game.run();
+
+        game.printWinner();
+    }
+
+    public void run() throws InterruptedException {
+        for(int i = 1; i < 101; i++){
+            move();
+            print();
+            Thread.sleep(200);
+        }
 
     }
+    public void move(){
+        for(Horse elem: horses){
+            elem.move();
+        }
+
+    }
+    public void print(){
+        for(Horse elem: horses){
+            elem.print();
+        }
+        for(int i=0; i < 10; i++){
+            System.out.println();
+        }
+    }
+    public Horse getWinner(){
+        double max = 0;
+        Horse winner = new Horse("",0.0,0.0);
+        for (Horse elem: horses){
+           if(elem.getDistance() > max){
+               max = elem.getDistance();
+               winner = elem;
+           }
+        }
+        return winner;
+    }
+    public void printWinner(){
+        System.out.println("Winner is " + getWinner().getName() + "!");
+    }
+
+
 }
